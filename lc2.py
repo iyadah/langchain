5a9cd4cd61282828ce5c0e09519979d192f5e311
iyad ahmad <63229668+iyadah@users.noreply.github.com>
2023-09-09 11:20:26 +0300

    Update lc2.py

--- lc2.py
+++ lc2.py
@@ -1,7 +1,7 @@
 import os
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
-os.environ['OPENAI_API_KEY']='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'
+os.environ['OPENAI_API_KEY']='sk-xx'
 print(os.environ['OPENAI_API_KEY'])
 llm = OpenAI(temperature=0.9)
 prompt = PromptTemplate(input_variables=['food'], template="What are 5 vacation destinations for someone who likes to eat {food}",)

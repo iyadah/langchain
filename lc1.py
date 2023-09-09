e214b8d9efce12c9e672255416cb87fae7c80055
iyad ahmad <63229668+iyadah@users.noreply.github.com>
2023-09-09 11:20:08 +0300

    Update lc1.py

--- lc1.py
+++ lc1.py
@@ -1,6 +1,6 @@
 import os
 from langchain.llms import OpenAI
-os.environ['OPENAI_API_KEY']='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'
+os.environ['OPENAI_API_KEY']='sk-xxxx'
 print(os.environ['OPENAI_API_KEY'])
 llm = OpenAI(temperature=0.9)
 text = "What are 5 vacation destinations for someone who likes to eat meat"
